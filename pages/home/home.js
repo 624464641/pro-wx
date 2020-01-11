5e8e8111e1581bc3f87f68fa63a4e733e1df09a0
xttong <admin@example.com>
2020-01-11 18:05:31 +0800

    初始化

---
+++ pages/home/home.js
@@ -0,0 +1,72 @@
+// pages/home/home.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    name:null,
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+  
+  /**
+   * 获取微信用户信息
+   */
+  getUserInfo(option){
+    let _thit = this;
+    _thit.showSettingToast("请授权"),
+    // 获取用户信息
+    wx.getSetting({
+      success(res) {
+        // console.log("res", res)
+        if (res.authSetting['scope.userInfo']) {
+          console.log("已授权=====")
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+          wx.getUserInfo({
+            success(res) {
+              console.log("获取用户信息成功", res)
+              _thit.setData({
+                name: res.userInfo.nickName
+              })
+            },
+            fail(res) {
+              console.log("获取用户信息失败", res)
+            }
+          })
+        } else {
+          console.log("未授权=====")
+          _thit.showSettingToast("请授权")
+        }
+      }
+    })
+    console.info(option)
+  },
+  // 打开权限设置页提示框
+  showSettingToast: function (e) {
+    wx.showModal({
+      title: '提示！',
+      confirmText: '去设置',
+      showCancel: false,
+      content: e,
+      success: function (res) {
+        if (res.confirm) {
+          wx.navigateTo({
+            url: '../setting/setting',
+          })
+        }
+      }
+    })
+  },
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
